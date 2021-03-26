0b09e6a38339fb3f7a25da1e8c434c6490cc11d5
savanahatieno <savanaht01@gmail.com>
2021-03-26 21:28:37 +0300

    Add delete button

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -4,7 +4,7 @@ import { Quote } from '../quote';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
-  styleUrls: ['./quotes.component.css']
+  styleUrls: ['./quotes.component.css'],
 })
 export class QuotesComponent implements OnInit {
   quotes: Quote[] = [
@@ -12,22 +12,23 @@ export class QuotesComponent implements OnInit {
       1,
       'Rick Falkvinge',
       'Bitcoin will do to banks what email did to the postal industry'
-    )
-  ]; 
+    ),
+  ];
   addNewQuote(quote: Quote) {
     let quoteLength = this.quotes.length;
     quote.id = quoteLength + 1;
     this.quotes.unshift(quote);
-  } 
+  }
   upVote(index: number) {
     this.quotes[index].upVotes += 1;
   }
   downVote(index: number) {
     this.quotes[index].downVotes += 1;
-  }  
-  constructor() { }
-
-  ngOnInit(): void {
   }
+  deleteQuote(index: number) {
+    this.quotes.splice(index, 1);
+  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
