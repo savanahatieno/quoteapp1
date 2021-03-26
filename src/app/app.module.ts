d306c0bf299d6a61685ca42d89ab4ee51eaed8a3
savanahatieno <savanaht01@gmail.com>
2021-03-26 15:19:11 +0300

    created quote  component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
