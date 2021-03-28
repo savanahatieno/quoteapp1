6efd786f3ef43bdf8fb60e44da945cc576d5c24b
savanahatieno <savanaht01@gmail.com>
2021-03-28 16:58:59 +0300

    Add date counter function and set a dsiplay of dates on the html

--- src/app/date-pip.pipe.ts
+++ src/app/date-pip.pipe.ts
@@ -1,12 +1,26 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'datePip'
+  name: 'datePip',
 })
 export class DatePipPipe implements PipeTransform {
+  transform(value: any): number {
+    let today: Date = new Date();
+    let todayWithNoTime: any = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate()
+    );
+    var dateDifference = Math.abs(todayWithNoTime - value);
+    const secondsInADay = 86400;
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    var dateDifferenceSeconds = dateDifference * 0.001;
+    var dateCounter = dateDifferenceSeconds / secondsInADay;
+
+    if (dateCounter >= 1 && todayWithNoTime > value) {
+      return dateCounter;
+    } else {
+      return 0;
+    }
   }
-
 }
